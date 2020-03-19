c7ec2a3d847be088c9a3c880045ac8eb8dec38b5
izumi <kizumi916@gmail.com>
2020-03-19 22:04:43 +0900

    投稿機能

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,6 +1,30 @@
 class SpotsController < ApplicationController
 
   def index
+    @spots = Spot.order("created_at DESC")
+    @images = Image.all
+    @spots = Spot.all
   end
   
+  def new
+    @spot = Spot.new
+    @spot.images.new
+    @spots = Spot.includes(:images).order('created_at DESC')
+  end
+
+  def create
+    @spot = Spot.new(spot_params)
+    if @spot.save
+      redirect_to root_path
+    else
+      @spot.images.new
+      render :new
+    end
+  end
+
+  private
+  def spot_params
+    params.require(:spot).permit(:prefecture_id, :name, :place, :text, :station, images_attributes: [:src, :_destroy, :id])
+  end
+
 end
