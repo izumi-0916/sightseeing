0648642d26e11fdf5a97976f605d5ef89cb0a4c2
izumi <kizumi916@gmail.com>
2020-03-20 13:05:53 +0900

    県ごと一覧ページ

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,8 +1,9 @@
 class Spot < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
-  belongs_to_active_hash :prefecture, foreign_key: true
+  belongs_to_active_hash :area, foreign_key: true
 
   validates :name,    presence: true
   validates :place,   presence: true
