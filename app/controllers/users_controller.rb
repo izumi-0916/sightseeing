129154072586bf4120a3aec4af1cccf37da29c0f
izumi <kizumi916@gmail.com>
2020-03-26 11:35:55 +0900

    mypageスクロールバー表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   def show
     user = User.find(params[:id])
-    @likes = Like.where(user_id: current_user.id).order("created_at DESC").page(params[:page]).per(9)
-    @spots = user.spots.order("created_at DESC").page(params[:page]).per(9)
+    @likes = Like.where(user_id: current_user.id).order("created_at DESC")
+    @spots = user.spots.order("created_at DESC")
   end
 end
