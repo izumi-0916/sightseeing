58f40250725cf495122f8dde24a6bfb3e71ba1b2
izumi <kizumi916@gmail.com>
2020-04-07 21:02:05 +0900

    エラーメッセージの順番

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -9,12 +9,12 @@ class Spot < ApplicationRecord
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to_active_hash :area, foreign_key: true
 
+  validates_associated :images
+  validates :images, presence: true
   validates :name,    presence: true
   validates :place,   presence: true
   validates :text,    presence: true
   validates :prefecture_id, numericality: { only_integer: true, greater_than: 0, less_than: 48 }
 
-  validates_associated :images
-  validates :images, presence: true
 
 end
